40d1ee3505a145514f251e7b11c2dca73b597f1e
Obinna Isiwekpeni <obinnaisiwekpeni@Obinnas-MacBook-Pro.local>
2021-06-19 14:33:36 +0200

    read more data from json

--- codes/utils.py
+++ codes/utils.py
@@ -1,3 +1,7 @@
 # get the data from the json file
 def read_data_from_json(data, number):
     return data['results']['root_node']['results'][number]['results']
+
+def get_property(data, arr, prop):
+    for i in range(0, len(data)):
+        arr.append(data[i][prop])
