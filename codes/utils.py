4f239ff5df10758e5aec17c081765680187c894f
Obinna Isiwekpeni <obinnaisiwekpeni@Obinnas-MacBook-Pro.local>
2021-06-21 17:14:26 +0200

    created function to add new df to list

--- codes/utils.py
+++ codes/utils.py
@@ -5,3 +5,17 @@ def read_data_from_json(data, number):
 def get_property(data, arr, prop):
     for i in range(0, len(data)):
         arr.append(data[i][prop])
+        
+        
+
+
+def get_list_of_dataframes(annotators, dataframe, reference_data_answers):
+    dataframes = []
+    for i in range(0, len(annotators)):
+        new_col = []
+        new_dataframe = dataframe[dataframe['Annotators'] == annotators[i]]
+        for j in range(0, len(new_dataframe['Images'])):
+            new_col.append(reference_data_answers[new_dataframe['Images'].iloc[j][:-4]])
+        new_dataframe['Reference'] = new_col
+        dataframes.append(new_dataframe)
+    return dataframes
